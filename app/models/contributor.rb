d382a3acc9e0b2cbb1593c9cf396fb92334bc740
Xolani <xmfuko@gmail.com>
2014-09-03 16:05:59 -0700

    has and belongs to many tags

--- app/models/contributor.rb
+++ app/models/contributor.rb
@@ -1,3 +1,4 @@
 class Contributor < ActiveRecord::Base
   validates :name, :title, :recipe, presence: true
+  has_and_belongs_to_many :tags
 end
--- spec/models/contributor_spec.rb
+++ spec/models/contributor_spec.rb
@@ -3,5 +3,6 @@ require 'rails_helper'
 describe Contributor do
   it { should validate_presence_of :name }
   it { should validate_presence_of :title }
-  it { should validate_presence_of :recipe}
+  it { should validate_presence_of :recipe }
+  it { should have_and_belong_to_many :tags }
 end
