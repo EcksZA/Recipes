09cfd3527e27e95c5d074d65bd3c3f201a7933a4
Xolani Mfuko <xmfuko@gmail.com>
2014-09-03 10:53:26 -0700

    create the show page for contributors

--- app/controllers/contributors_controller.rb
+++ app/controllers/contributors_controller.rb
@@ -3,4 +3,9 @@ class ContributorsController < ApplicationController
     @contributor = Contributor.all
     render('contributors/index.html.erb')
   end
+
+  def show
+    @contributor = Contributor.find(params[:id])
+    render('contributors/show.html.erb')
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
   match('/contributors', {:via => :get, :to => 'contributors#index'})
+  match('/contributors/:id', {:via => :get, :to => 'contributors#show'})
 end
