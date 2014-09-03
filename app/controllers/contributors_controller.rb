c3a3ce6c1d2cdfd451fb76d69d7c264af0831546
Xolani Mfuko <xmfuko@gmail.com>
2014-09-03 10:49:16 -0700

    create the index page for contributors

--- app/controllers/contributors_controller.rb
+++ app/controllers/contributors_controller.rb
@@ -1,2 +1,6 @@
 class ContributorsController < ApplicationController
+  def index
+    @contributor = Contributor.all
+    render('contributors/index.html.erb')
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,3 @@
 Rails.application.routes.draw do
-
+  match('/contributors', {:via => :get, :to => 'contributors#index'})
 end
