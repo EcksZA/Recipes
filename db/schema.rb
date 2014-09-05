abb73d1dfcca9b92440954ea2f61ed8aa33b5792
Xolani <xmfuko@gmail.com>
2014-09-05 09:49:02 -0700

    rename the join table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140903225105) do
+ActiveRecord::Schema.define(version: 20140905164009) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,7 +22,7 @@ ActiveRecord::Schema.define(version: 20140903225105) do
     t.text   "recipe"
   end
 
-  create_table "contributors_tags", id: false, force: true do |t|
+  create_table "taggables", id: false, force: true do |t|
     t.integer "contributor_id", null: false
     t.integer "tag_id",         null: false
   end
