ad04d9b3ce9d140107b793f8137bbb31b138b03b
Xolani <xmfuko@gmail.com>
2014-09-05 16:02:47 -0700

    test uniqueness of an entry into the taggable table, so that only one contributor-tag relationship will exist

--- app/models/taggable.rb
+++ app/models/taggable.rb
@@ -3,4 +3,5 @@ class Taggable < ActiveRecord::Base
   belongs_to :tag
   validates :contributor_id, presence: true
   validates :tag_id, presence: true
+  validates_uniqueness_of :contributor_id, scope: :tag_id
 end
--- spec/models/taggable_spec.rb
+++ spec/models/taggable_spec.rb
@@ -5,4 +5,5 @@ describe Taggable do
   it { should belong_to :tag }
   it { should validate_presence_of :contributor_id }
   it { should validate_presence_of :tag_id }
+  it { should validate_uniqueness_of(:contributor_id).scoped_to(:tag_id) }
 end
