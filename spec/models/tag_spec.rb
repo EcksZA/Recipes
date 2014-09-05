90afd9152ac140c95f1a65d09a8ed7f971229a22
Xolani <xmfuko@gmail.com>
2014-09-05 13:57:39 -0700

    have many taggables

--- spec/models/tag_spec.rb
+++ spec/models/tag_spec.rb
@@ -2,5 +2,5 @@ require 'rails_helper'
 
 describe Tag do
   it { should validate_presence_of :name }
-  it { should have_and_belong_to_many :contributors }
+  it { should have_many :taggables }
 end
