e0f975aa93a053c697d2f1805853bfbe800e9b8a
Xolani <xmfuko@gmail.com>
2014-09-05 13:25:14 -0700

    have many taggables

--- spec/models/contributor_spec.rb
+++ spec/models/contributor_spec.rb
@@ -4,5 +4,5 @@ describe Contributor do
   it { should validate_presence_of :name }
   it { should validate_presence_of :title }
   it { should validate_presence_of :recipe }
-  it { should have_and_belong_to_many :tags }
+  it { should have_many :taggables }
 end
