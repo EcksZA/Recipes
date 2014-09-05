2726686934dd82e17a76e320767bbb0214d6a51d
Xolani <xmfuko@gmail.com>
2014-09-05 12:45:28 -0700

    fix an error in the contributor_spec file

--- spec/models/contributor_spec.rb
+++ spec/models/contributor_spec.rb
@@ -5,7 +5,4 @@ describe Contributor do
   it { should validate_presence_of :title }
   it { should validate_presence_of :recipe }
   it { should have_and_belong_to_many :tags }
-
-  context '#tagging' do
-    it ""
 end
