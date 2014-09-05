6017e12ff69426e2cd5423db9a68b450981346b1
Xolani <xmfuko@gmail.com>
2014-09-05 14:14:40 -0700

    have many tags through taggables

--- app/models/contributor.rb
+++ app/models/contributor.rb
@@ -1,4 +1,5 @@
 class Contributor < ActiveRecord::Base
   validates :name, :title, :recipe, presence: true
   has_many :taggables
+  has_many :tags, through: :taggables
 end
--- spec/models/contributor_spec.rb
+++ spec/models/contributor_spec.rb
@@ -5,4 +5,5 @@ describe Contributor do
   it { should validate_presence_of :title }
   it { should validate_presence_of :recipe }
   it { should have_many :taggables }
+  it { should have_many(:tags).through(:taggables) }
 end
